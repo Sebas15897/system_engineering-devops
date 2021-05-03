da37f1a367c3f0771f4e6565d0ed2ad4347b9f8e
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-05-03 17:39:42 -0500

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,23 +12,32 @@ if __name__ == '__main__':
     except ValueError:
         exit()
 
+    # Main formatted names to API uris and filenames
     api_url = 'https://jsonplaceholder.typicode.com'
     user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
     todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
     filename = '{emp_id}.csv'.format(emp_id=emp_id)
 
+    # User Response
     res = requests.get(user_uri).json()
 
+    # Username of the employee
     username = res.get('username')
 
+    # User TODO Response
     res = requests.get(todo_uri).json()
 
+    # Create the new file for the user to save the information
+    # Filename example: `{user_id}.csv`
     with open(filename, 'w', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
 
         for elem in res:
+            # Completed or non-completed task
             status = elem.get('completed')
 
+            # The task name
             title = elem.get('title')
 
+            # Writing each result of API response in a row of a CSV file
             writer.writerow([emp_id, username, status, title])
