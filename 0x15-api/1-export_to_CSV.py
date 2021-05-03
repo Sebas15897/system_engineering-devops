711e2d3f9af0585ff9faec9c8a631c3a95b86236
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-05-03 17:42:22 -0500

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""
-return all of the information using API request
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-def get_information(user_id):
-    """returns information about his/her TODO list progress
-    exporting data in the CSV format.
-    """
-    response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
-    response_json = response.json()
-    name = response_json.get('username')
-
-    response_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-            user_id))
-    r_json_todos = response_todos.json()
-
-    file_csv = "{}.csv".format(user_id)
-    with open(file_csv, 'a') as fd:
-        for task in r_json_todos:
-            csv_data = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
-                user_id, name,
-                task.get('completed'), task.get('title'))
-            fd.write(csv_data)
-
-
-if __name__ == '__main__':
-    get_information(argv[1])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
