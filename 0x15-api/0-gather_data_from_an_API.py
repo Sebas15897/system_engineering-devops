0031e07f0849ecf5b8aa9de64c134f4ca0da78a9
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-05-03 17:37:24 -0500

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,29 +15,21 @@ if __name__ == '__main__':
     user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
     todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
 
-
     res = requests.get(user_uri).json()
-
 
     name = res.get('name')
 
-
     res = requests.get(todo_uri).json()
-
 
     total = len(res)
 
-    
     non_completed = sum([elem['completed'] is False for elem in res])
 
-    
     completed = total - non_completed
 
-    
     str = "Employee {emp_name} is done with tasks({completed}/{total}):"
     print(str.format(emp_name=name, completed=completed, total=total))
 
-    
     for elem in res:
         if elem.get('completed') is True:
             print('\t', elem.get('title'))
